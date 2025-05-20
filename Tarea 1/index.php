dac276fd062cf9c88315ba42140f7f96b3c869cf
ElRay32 <guerrero20191@outlook.es>
2025-05-20 12:32:11 -0400

    Tarea 1
    creando las bases de la tarea 1

---
+++ Tarea 1/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>uno</title>
+        <style>
+            
+            body{
+                font-family: arial, sans-serif;
+                background-color: white;
+                margin: 0;
+                padding: 0;
+            }
+            #tarea1{
+                width: auto;
+                max-width: 1000px;
+                margin: 0 auto;
+                background-color: #fff;
+                padding: 20px;
+                border-radius: 5px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+
+            #idmenu{
+               margin: 20px 0;
+            }
+
+            #idmenu ul{
+                list-style-type: none;
+                padding: 0;
+            }
+
+            #idmenu li{
+                display: inline;
+                margin-right: 20px;
+            }
+
+            #idmenu a{
+                text-decoration: none;
+                color: #333;
+                font-weight: bold;
+            }
+
+            #idcontenido{
+
+            }
+            
+        </style>
+    </head>
+    <body>
+        <div id="tarea1">
+
+        <div>
+            <h1>Tarea 1</h1>
+            <p>uno dos</p>
+        </div>
+
+        <hr>
+        <div id="idmenu">
+            <ul>
+                <li>
+                    <a href="mi_tarjeta.php">Mi Tarjeta</a>
+                </li>
+                <li>
+                    <a href="calculadora.php">Calculadora</a>
+                </li>
+                <li>
+                    <a href="adivina.php">Adivina</a>
+                </li>
+                <li>
+                    <a href="acerca_de.php">Acerca de Mi</a>
+                </li>
+            </ul>
+        </div>
+        <div id="idcontenido">
+
+        </div>
+        <div id="idfooter">
+            <h5></h5>
+        </div>
+        </div>
+    </body>
+</html>
